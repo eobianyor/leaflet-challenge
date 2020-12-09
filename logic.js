ca465811c759c09634db53737117e298b84841e1
Emeka Obianyor <obianyore@gmail.com>
2020-12-08 19:07:14 -0600

    got legend to show up

--- logic.js
+++ logic.js
@@ -135,15 +135,16 @@ function createMap(earthquakes, plates) {
   legend.onAdd = function (myMap) {
 
     var div = L.DomUtil.create('div', 'info legend');
-    labels = ['<strong>Depths</strong>'],
-      depths = ['> 250ft', '> 150ft', '> 100ft', '> 50ft', '< 50ft'];
+    labels = ['<strong>Depths</strong>'];
+    var depths = ['> 250ft', '> 150ft', '> 100ft', '> 50ft', '< 50ft'];
+    var Lcolors = ["#19F52A", "#DF554D", "#DF554D", "#A31A0E", "#661400"]
 
     for (var i = 0; i < depths.length; i++) {
 
       div.innerHTML +=
         labels.push(
-          '<i class="square" style="background:' + getColor(depths[i]) + '"></i> ' +
-          (depths[i] ? depths[i] : '+'));
+          '<i class="square" style="background:' + (Lcolors[i]) + '"></i> ' +
+          depths[i] + (depths[i + 1] ? "ndash;" + depths[i + 1] + "<br>" : '+'));
 
     }
     div.innerHTML = labels.join('<br>');
